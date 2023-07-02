863b7af3d1d79c1737d55a65cb474d869bf1e743
Kim soyoung <3001th0@naver.com>
2023-07-02 15:29:15 +0900

    css 파일 style 표기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,15 +2,7 @@ import './App.css';
 
 function App() {
   const name = '리액트';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: '48px',
-    fontWeight: 'bold',
-    padding: 16,//기본 단위는 px
-    // key : value,
-  }
-  return <div style={style}>{name}</div>;
+  return <div className='react'>{name}</div>;
 }
 
 export default App;
