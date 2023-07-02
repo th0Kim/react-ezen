f4eeedd92551077ef44e3a479d46575e6c1ebb2b
Kim soyoung <3001th0@naver.com>
2023-07-02 15:52:14 +0900

    주석

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,7 +2,17 @@ import './App.css';
 
 function App() {
   const name = '리액트';
-  return <div className='react'>{name}</div>;
+  return (
+    // 이 공간은 자바스크립트 공간입니다.
+    /* 
+    주석으로 처리하는 공간이
+    여러줄로 되어 있으면
+    주석이 달리 표현됩니다.
+    */
+    <div>
+      <div className='react'>{name}</div>
+      {/* 이 공간은 jsx문법 공간입니다. */}
+    </div>
+  );
 }
-
 export default App;
