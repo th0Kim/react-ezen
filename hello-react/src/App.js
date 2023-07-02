40cb5f4f95688a3465cd570b0432a13239a7e729
Kim soyoung <3001th0@naver.com>
2023-07-02 14:38:39 +0900

    숫자값으로 조건연산자 처리 시 false는 0으로 출력 됨

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   // return 메서드 안에서 쓸 수 있는 전역 변수 위치
-  const number = "0";
-  return <>{number === '리액트' && <h1>리액트입니다</h1>}</>
+  const number = 0;
+  return <div>{number && <h1>리액트입니다</h1>}</div>;
 }
 
 export default App;
