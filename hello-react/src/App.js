1dd1c698daf014587067780ae93cc8831b61ba32
Kim soyoung <3001th0@naver.com>
2023-07-02 15:26:39 +0900

    inline style 표기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,16 @@
-// import { Fragment } from 'react';
 import './App.css';
 
 function App() {
-  const name = undefined;
-  return <div>{name || '리액트'}</div>
+  const name = '리액트';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '48px',
+    fontWeight: 'bold',
+    padding: 16,//기본 단위는 px
+    // key : value,
+  }
+  return <div style={style}>{name}</div>;
 }
 
 export default App;
