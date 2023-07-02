1cae2b3c25cb3fbb69625019e60ac966484553d5
Kim soyoung <3001th0@naver.com>
2023-07-02 14:31:06 +0900

    조건부 연산자

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,15 +3,8 @@ import './App.css';
 
 function App() {
   // return 메서드 안에서 쓸 수 있는 전역 변수 위치
-  const name = "리액트";
-  return (
-    <>
-    {/* <></>도 Fragment와 동일한 컨테이너로 사용 가능 */}
-    
-      <h1>Hello { name }</h1>
-      <h2>난 소영쓰 !</h2>
-    </>
-  );
+  const number = "0";
+  return <>{number === '리액트' && <h1>리액트입니다</h1>}</>
 }
 
 export default App;
