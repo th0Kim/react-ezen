10c30c766dab218c50367b6f9cba9896aa2a6c8b
Kim soyoung <3001th0@naver.com>
2023-07-09 11:18:00 +0900

    name값을  string으로 사용하여 오류 제거

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -6,7 +6,7 @@ import MyComponent from './MyComponent';
 const App = () => {
   return (
     <div>
-      <MyComponent name={3}>
+      <MyComponent name={'react'}>
         리액트 {/* App의 MyComponent 자식 */}
       </MyComponent>
     </div>
