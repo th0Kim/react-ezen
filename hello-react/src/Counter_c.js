c6555ee9df890682c82eff4155f92bf03ae7e409
Kim soyoung <3001th0@naver.com>
2023-07-09 12:26:13 +0900

    this 누구냠

--- hello-react/src/Counter_c.js
+++ hello-react/src/Counter_c.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class Counter_c extends Component {
   constructor(props) {
     super(props);
+    // console.log(this)
     this.state = {
       number: 0,
     }
@@ -12,7 +13,7 @@ class Counter_c extends Component {
     const { number } = this.state;
     return (
       <div>
-        <p>{number}</p>
+        <h4>{number}</h4>
         <button onClick={() => {this.setState({ number: number + 1 });}}>
           +1
         </button>
