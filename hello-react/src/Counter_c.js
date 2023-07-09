bc8a5fe250b3a55d1df9e17429fcdd01d874ae17
Kim soyoung <3001th0@naver.com>
2023-07-09 14:02:38 +0900

    다음을 위한 정리

--- hello-react/src/Counter_c.js
+++ hello-react/src/Counter_c.js
@@ -12,12 +12,9 @@ class Counter_c extends Component {
       <div>
         <h4>{number}</h4>
         <h5>바뀌지 않는 값: {fixedNumber}</h5>
-        {/* this.setState을 사용해서 값을 변화 시켜준다 */}
         <button onClick={() => {
-          //this.setState({number: number + 1}); //이전 값 없이 초기값 + 1 만 연산 한다.
-          //prev : 이전 값을 저장 함으로 값을 누적하여 사용 할 수 있다.
-          this.setState((prev) => ({ number: prev.number + 1 }));
-          this.setState((prev) => ({ number: prev.number + 1 }));
+          //this.setState({number: number + 1});
+          // this.setState((prev) => ({ number: prev.number + 1 }));
           }}>
           +1
         </button>
