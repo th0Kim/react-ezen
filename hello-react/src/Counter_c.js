b408584d635caf6869ea05d6c4e4615bcefb507b
Kim soyoung <3001th0@naver.com>
2023-07-09 12:42:17 +0900

    this.setState 사용 방법

--- hello-react/src/Counter_c.js
+++ hello-react/src/Counter_c.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 
 class Counter_c extends Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      number: 0,//초기값
+    super(props);//1. 부모로 부터 props 속성 값을 상속을 받아서
+    this.state = { //2. 그 중에 this.state를 변화 시켜줄거고
+      number: 0,//3. number만 변화를 주기 위한 초기값을 준다. 
     }
   }
 
   render() {
-    const { number } = this.state; //상태값
+    const { number } = this.state; //3. 
     return (
       <div>
         <h4>{number}</h4>
-        <button onClick={() => {this.setState({ number: number + 1 });}}>
+        <button onClick={() => {this.setState({ number: number + 1 });}}>{/* 4. this.setState 변화 함수 사용 시 초기값이 필요하여 number를 0으로 this.state로 초기값 선언 */}
           +1
         </button>
       </div>
