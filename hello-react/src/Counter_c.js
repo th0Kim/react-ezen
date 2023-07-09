e96e2bb586d2f6117fbdcb8e840c22a1f2e4480a
Kim soyoung <3001th0@naver.com>
2023-07-09 12:53:57 +0900

    설명 정리

--- hello-react/src/Counter_c.js
+++ hello-react/src/Counter_c.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
 class Counter_c extends Component {
-  state = { //1. 부모로 부터 props 속성 값을 상속을 받아서 아래 나오는 애들 변화 해줄 거임
-    number: 0,//2. number만 변화를 주기 위한 초기값을 준다. 
+  state = { // 초기값 설정은 state로 해준다
+    number: 0,
     fixedNumber: 0,
   }
 
@@ -12,7 +12,7 @@ class Counter_c extends Component {
       <div>
         <h4>{number}</h4>
         <h5>바뀌지 않는 값: {fixedNumber}</h5>
-        <button onClick={() => {this.setState({ number: number + 1 });}}>{/* 3. this.setState 변화 함수 사용 시 초기값이 필요하여 number를 0으로 this.state로 초기값 선언 */}
+        <button onClick={() => {this.setState({ number: number + 1 });}}>{/* this.setState을 사용해서 값을 변화 시켜준다 */}
           +1
         </button>
       </div>
