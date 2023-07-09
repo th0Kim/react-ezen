bace9138da84d325dacd8331df6fea0b5d57df3a
Kim soyoung <3001th0@naver.com>
2023-07-09 14:59:39 +0900

    useState setColor 사용 예시

--- hello-react/src/Say.js
+++ hello-react/src/Say.js
@@ -8,11 +8,20 @@ const Say = () => {
   const onClickEnter = () => setMassage("hello~");
   const onClickLeave = () => setMassage("bye bye");
 
+  const [color, setColor] = useState('black'); //초기값
+
   return (
     <div>
+      {/* JSX문법 */}
       <button onClick={onClickEnter}>입장</button>
       <button onClick={onClickLeave}>퇴장</button>
-      <h1>{message}</h1>
+
+      <h1 style={{ color }}>{message}</h1>{/* style 괄호 안 {}은 변수다, 초기값 'black */}
+
+      {/* style={{ color: '컬러명' }} 은 단순 button 텍스트 컬러 */}
+      <button style={{ color: 'red' }} onClick={() => setColor('red')}>red</button>
+      <button style={{ color: 'green' }} onClick={() => setColor('green')}>green</button>
+      <button style={{ color: 'blue' }} onClick={() => setColor('blue')}>blue</button>
     </div>
   )
 };
