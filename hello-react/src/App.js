21b361d4da2e6588be7b4cb155d0409610fe092e
Kim soyoung <3001th0@naver.com>
2023-07-09 11:47:54 +0900

    함수형/class형 둘 다 노출 시킴

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 
 import React from 'react';
-// import MyComponent from './MyComponent'; //함수형 lint 약어 rcc
+import MyComponent from './MyComponent'; //함수형 lint 약어 rcc
 import MyComponentC from './MyComponent_c'; //class형 lint 약어 rsc
 
 const App = () => {
   return (
     <div>
-      <MyComponentC name={'react'} favoriteNumber={1}>
+      <h3>함수형</h3>
+      <MyComponent name={'react'} favoriteNumber={1}>
+        리액트 {/* App의 MyComponent 자식 */}
+      </MyComponent>
+
+      <h3>Class형</h3>
+      <MyComponentC name={'react'} favoriteNumber={3}>
         리액트 {/* App의 MyComponentC 자식 */}
       </MyComponentC>
     </div>
