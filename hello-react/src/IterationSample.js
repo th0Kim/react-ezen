2c1f6201881ffdc25b69d64722bbdbb8915f58d9
Kim soyoung <3001th0@naver.com>
2023-07-16 15:54:05 +0900

    반복 시키기 .concat으로 항목 추가 하고 인풋 초기화

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const IterationSample = () => {
-	const names = ['눈사람', '얼음', '눈', '바람'];
-	const nameList = names.map((name, idx) => <li key={idx}>{name}</li>);
+	const [names, setNames] = useState([// 초기값
+		{id: 1, text: '눈사람'},// key value
+		{id: 2, text: '얼음'},// key value
+		{id: 3, text: '눈'},// key value
+		{id: 4, text: '바람'},// key value
+	])
 
-  return <ul>{nameList}</ul>;
+	const [inputText, setInputText] = useState('');
+	const [nextId, setNextId] = useState(5);
+
+	const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
+
+	const onChange = e => setInputText(e.target.value);
+
+	const onClick = () => {
+		const nextNames = names.concat({
+			id: nextId, 
+			text: inputText
+		});
+
+		setNextId(nextId + 1); //추가 할 id가 1씩 증가
+		setNames(nextNames);//추가 할 이름
+		setInputText('');//추가 후 인풋 초기화
+	}
+	
+  const onKeyPress = e => {
+    if( e.key === 'Enter' ) {
+      onClick();
+    }
+  }
+
+  return (
+		<>
+			<input value={inputText} onChange={onChange} onKeyDown={onKeyPress}/>
+			<button onClick={onClick}>추가</button>
+			<ul>{nameList}</ul>
+		</>
+	);
 };
 
 export default IterationSample;
