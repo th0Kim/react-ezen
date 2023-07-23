936ce037b540b0bb48727d5f5b95c1ffdc6cfadf
Kim soyoung <3001th0@naver.com>
2023-07-23 17:21:13 +0900

    TodoList item반복 map 적용

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,13 +1,32 @@
+import { useState } from 'react';
 import './App.css';
 import TodoTemplate from './component/TodoTemplate';
 import TodoInsert from './component/TodoInsert';
 import TodoList from './component/TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링 해보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: false,
+    },
+  ]);
+
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 }
