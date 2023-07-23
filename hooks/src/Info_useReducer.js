b9601718b8998b6a9561f5e87a62e9840ef2289b
Kim soyoung <3001th0@naver.com>
2023-07-23 16:01:49 +0900

    useInputs.js 함수 사용 적용하기

--- hooks/src/Info_useReducer.js
+++ hooks/src/Info_useReducer.js
@@ -1,40 +1,24 @@
 import React, { useReducer } from "react";
-
-function reducer(state, action) {
-	return {
-		...state,//불변성 때문에 이전의 상태를 복사
-		[action.name]:action.value//key값을 []로 하면 key 해당하는 value값을 인지 할 수 있다. ex) name="", value=""
-		// action.name 은 name=""을 가르킴 
-	}
-}
+import useInputs from "./useInputs";
 
 const Info = () => {
-	const [state, dispatch] = useReducer(reducer, {
-		myname : '',
-		nickname : '',
-		adress : ''
+	const [state, onChange] = useInputs({
+		name: '',
+		nickname: ''
 	});
-	// [현재상태, 액션 발생시키는 함수] = useReducer(리듀서함수, 해당 리듀서의 기본값);
 
-	const {myname, nickname, adress} = state;//비구조할당문
-
-	const onChange = (e) => {
-		dispatch(e.target);
-	}
+	const {name, nickname} = state;
 
   return (
 		<div>
 			<div>
-				<input name="myname" value={myname} onChange={onChange} placeholder='이름' />
+				<input name="name" value={name} onChange={onChange} placeholder='이름' />
 				<br/>
 				<input  name="nickname" value={nickname} onChange={onChange} placeholder='닉네임' />
-				<br/>
-				<input  name="adress" value={adress} onChange={onChange} placeholder='주소' />
 			</div>
 			<div>
-				<div><b>이름:</b> {myname}</div>
+				<div><b>이름:</b> {name}</div>
 				<div><b>닉네임:</b> {nickname}</div>
-				<div><b>주소:</b> {adress}</div>
 			</div>
 		</div>
 	);
--- hooks/src/useInputs.js
+++ hooks/src/useInputs.js
@@ -1,1 +1,18 @@
+import { useReducer } from "react";
+
+function reducer(state, action) {
+	return {
+		...state,
+		[action.name]:action.value
+	}
+}
+
+export default function useInputs(initialForm){
+	const [state, dispatch] = useReducer(reducer, initialForm);
+	const onChange = (e) => {
+		dispatch(e.target);
+	}
+  return [state, onChange];
+}
+
 //얘는 함수래..그래서 파일명 앞에 소문자임..암튼 그럼
