bc3f40aca012db43e8ecd0ee5836d9c96c92a021
Kim soyoung <3001th0@naver.com>
2023-07-23 12:27:54 +0900

    Info.js를 useReducer로 변형

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -3,11 +3,13 @@ import React, {useState} from "react";
 
 import Counter from './Counter'; //useState
 import CounterUseReducer from './Counter_useReducer'; //useReducer
+import InfoUseReducer from './Info_useReducer'; //useReducer
 import Info from './Info'; //useState
 
 
 const App = () => {
   const [visible, setVisible] = useState(false); // boolean이 되도록 false, true 상태로 초기화
+  const [visible2, setVisible2] = useState(false); // boolean이 되도록 false, true 상태로 초기화
 
   return (
     <div>
@@ -20,6 +22,14 @@ const App = () => {
       </button>
       <hr />
       { visible && <Info /> }{/* visible이 true이면 <Info/> 출력 */}
+      
+
+      <h2>useReducer를 여러번 사용하기</h2>
+      <button onClick={() => {setVisible2(!visible2);}}> {/* 클릭 할 때 마다 */}
+        {visible2 ? '숨기기' : '보이기'}{/* visible이 true이면 버튼 문구가 '숨기기', false이면 '보이기' */}
+      </button>
+      <hr />
+      { visible2 && <InfoUseReducer /> }{/* visible이 true이면 <Info/> 출력 */}
     </div>
   );
 }
