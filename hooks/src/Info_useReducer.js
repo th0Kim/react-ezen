a1980897b823dbd4d3c2d96087d28c31ef34e70b
Kim soyoung <3001th0@naver.com>
2023-07-23 12:39:28 +0900

    useReducer : onChange 상태값 변경

--- hooks/src/Info_useReducer.js
+++ hooks/src/Info_useReducer.js
@@ -2,17 +2,20 @@ import React, { useReducer } from "react";
 
 function reducer(state, action) {
 	return {
-		...state,
-		[action.name]:action.value
+		...state,//불변성 때문에 이전의 상태를 복사
+		[action.name]:action.value//key값을 []로 하면 key 해당하는 value값을 인지 할 수 있다. ex) name="", value=""
+		// action.name 은 name=""을 가르킴 
 	}
 }
 
 const Info = () => {
 	const [state, dispatch] = useReducer(reducer, {
-		name : '',
+		myname : '',
 		nickname : ''
 	});
-	const {name, nickname} = state;
+	// [현재상태, 액션 발생시키는 함수] = useReducer(리듀서함수, 해당 리듀서의 기본값);
+
+	const {myname, nickname} = state;//비구조할당문
 
 	const onChange = (e) => {
 		dispatch(e.target);
@@ -21,12 +24,12 @@ const Info = () => {
   return (
 		<div>
 			<div>
-				<input name="name" value={name} onChange={onChange} placeholder='이름' />
+				<input name="myname" value={myname} onChange={onChange} placeholder='이름' />
 				<br/>
 				<input  name="nickname" value={nickname} onChange={onChange} placeholder='닉네임' />
 			</div>
 			<div>
-				<div><b>이름:</b> {name}</div>
+				<div><b>이름:</b> {myname}</div>
 				<div><b>닉네임:</b> {nickname}</div>
 			</div>
 		</div>
