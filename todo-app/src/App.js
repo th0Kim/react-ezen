b8ca4383d27196842bf0ca010e52a8a659cc64e6
Kim soyoung <3001th0@naver.com>
2023-07-30 13:44:56 +0900

    createBulkTodos 데이터 추가

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -4,26 +4,40 @@ import TodoTemplate from './component/TodoTemplate';
 import TodoInsert from './component/TodoInsert';
 import TodoList from './component/TodoList';
 
-function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트의 기초알아보기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링 해보기',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '일정 관리 앱 만들어 보기',
+function createBulkTodos() {
+  const array = [];
+  for( let i = 1; i <= 2500; i++) {// 2500개 데이터 한번에 불러오면 성능에 안좋지
+    array.push({//concat이 아닌 이유 : 그냥 데이터가 필요해서.. 복사 해올게 없다
+      id: i,
+      text: `할일 ${i}`,
       checked: false,
-    },
-  ]);
+    });
+  }
 
-  const nextId = useRef(4);//nextId를 지역변수로 이 컴포넌트에서만 사용한다.
+  return array;
+}
+
+const App = () => {
+  // const [todos, setTodos] = useState([
+  //   {
+  //     id: 1,
+  //     text: '리액트의 기초알아보기',
+  //     checked: true,
+  //   },
+  //   {
+  //     id: 2,
+  //     text: '컴포넌트 스타일링 해보기',
+  //     checked: true,
+  //   },
+  //   {
+  //     id: 3,
+  //     text: '일정 관리 앱 만들어 보기',
+  //     checked: false,
+  //   },
+  // ]);
+  const [todos, setTodos] = useState(createBulkTodos);//함수를 파라미터 값으로 넘기면 초기 랜더링 시 한번만 실행 된다.
+
+  const nextId = useRef(2501);//nextId를 지역변수로 이 컴포넌트에서만 사용한다.
   const onInsert = useCallback((text) => {//text 값만 받아오기
     const todo = {
       id: nextId.current,//4
