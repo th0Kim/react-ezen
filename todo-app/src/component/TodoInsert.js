74cb6338ce691b3c9792a36c426409e42df2a597
Kim soyoung <3001th0@naver.com>
2023-07-30 11:50:46 +0900

    onSubmit 함수를 사용하여 value에 값을 listItem에 업데이트

--- todo-app/src/component/TodoInsert.js
+++ todo-app/src/component/TodoInsert.js
@@ -1,10 +1,25 @@
 import { IoIosAdd } from 'react-icons/io';
 import './TodoInsert.scss';
+import { useCallback, useState } from 'react';
 
-const TodoInsert = () => {
+const TodoInsert = ({onInsert}) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  },[]);//처음에만 랜더링해서 함수 최적화가 됨
+
+  // submit이 발생 할 때 onInsert가 실행
+  const onSubmit = useCallback((e) => {
+    onInsert(value);
+    setValue('');
+    e.preventDefault();
+  },[onInsert, value],)
+
+  
   return (
-    <form className="TodoInsert">
-      <input placeholder="할 일을 입력하세요." />
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input placeholder="할 일을 입력하세요." value={value} onChange={onChange} />
       <button type="submit">
         <IoIosAdd />
       </button>
