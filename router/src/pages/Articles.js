769d685e4c6de7ba70c5b83f66b21ee1bd00ed75
Kim soyoung <3001th0@naver.com>
2023-08-06 15:33:34 +0900

    NavLink 간소화 1

--- router/src/pages/Articles.js
+++ router/src/pages/Articles.js
@@ -2,42 +2,35 @@ import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
 
 const Articles = () => {
+  return (
+    <>
+      <ul>
+        <ArticleItem id={1} />
+        <ArticleItem id={2} />
+        <ArticleItem id={3} />
+        <ArticleItem id={4} />
+      </ul>
+      {/* Outlet은 <Route path=":id" element={<Article />} /> */}
+      <Outlet />
+    </>
+  );
+};
+
+const ArticleItem = ({ id }) => {
   const activeStyle = {
     color: "green",
     fontSize: 21,
   };
   return (
-    <>
-      <ul>
-        <li>
-          {/* <Route path="/articles/:id" element={<Articles />} /> */}
-          <NavLink
-            to="/articles/1"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            게시글 1
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/articles/2"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            게시글 2
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/articles/3"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            게시글 3
-          </NavLink>
-        </li>
-      </ul>
-      {/* Outlet은 <Route path=":id" element={<Article />} /> */}
-      <Outlet />
-    </>
+    <li>
+      {/* <Route path="/articles/:id" element={<Articles />} /> */}
+      <NavLink
+        to={`/articles/${id}`}
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+      >
+        게시글 {id}
+      </NavLink>
+    </li>
   );
 };
 
