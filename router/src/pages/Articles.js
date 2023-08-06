0cb35afccf25327efb68eedb52b102b07610f0f8
Kim soyoung <3001th0@naver.com>
2023-08-06 14:45:45 +0900

    Outlet 사용

--- router/src/pages/Articles.js
+++ router/src/pages/Articles.js
@@ -1,20 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Articles = () => {
   return (
-    <ul>
-      <li>
-        {/* <Route path="/articles/:id" element={<Articles />} /> */}
-        <Link to="/articles/1">게시글 1</Link>
-      </li>
-      <li>
-        <Link to="/articles/2">게시글 2</Link>
-      </li>
-      <li>
-        <Link to="/articles/3">게시글 3</Link>
-      </li>
-    </ul>
+    <>
+      {/* Outlet은 <Route path=":id" element={<Article />} /> */}
+      <Outlet />
+      <ul>
+        <li>
+          {/* <Route path="/articles/:id" element={<Articles />} /> */}
+          <Link to="/articles/1">게시글 1</Link>
+        </li>
+        <li>
+          <Link to="/articles/2">게시글 2</Link>
+        </li>
+        <li>
+          <Link to="/articles/3">게시글 3</Link>
+        </li>
+      </ul>
+    </>
   );
 };
 
