899164e1bb3d1fc5861d007f8f8402475d0e63ac
Kim soyoung <3001th0@naver.com>
2023-08-06 12:55:37 +0900

    URL 파라미터

--- router/src/pages/Home.js
+++ router/src/pages/Home.js
@@ -6,7 +6,21 @@ const Home = () => {
     <div>
       <h1>Home</h1>
       <p>가장 먼저 보여지는 페이지</p>
-      <Link to="/about">About</Link>
+
+      <ul>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/profile/juho">주호의 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/miju">미주의 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/void">존재하지 않는 프로필</Link>
+        </li>
+      </ul>
     </div>
   );
 };
