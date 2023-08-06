bf97a8059502fcb23f4859d275408921ca9536dd
Kim soyoung <3001th0@naver.com>
2023-08-06 15:09:28 +0900

    useNavigate 사용방법

--- router/src/Layout.js
+++ router/src/Layout.js
@@ -1,13 +1,24 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const Layout = () => {
+  // Link 컴포넌트를 사용하지 않고 다른 페이지로 이동해야 하는 상황에서 사용하는 hook
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+  const goAtricles = () => {
+    navigate("/articles");
+  };
+
   return (
     <div>
       <header
         style={{ backgroundColor: "cornflowerblue", padding: 16, fontSize: 24 }}
       >
-        header
+        <button onClick={goBack}>뒤로가기</button>
+        <button onClick={goAtricles}>게시글 목록</button>
       </header>
       <main>
         <Outlet />
