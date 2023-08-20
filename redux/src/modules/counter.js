7c0544efe66b72943cecd122ba2157971f6eddeb
Kim soyoung <3001th0@naver.com>
2023-08-20 16:25:12 +0900

    redux-actions 문법으로 변환

--- redux/src/modules/counter.js
+++ redux/src/modules/counter.js
@@ -1,10 +1,16 @@
+import { createAction, handleActions } from "redux-actions";
+
 // 액션 객체 생성
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
 
 // 액션 생성 함수
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+// export const increase = () => ({ type: INCREASE });
+// export const decrease = () => ({ type: DECREASE });
+
+// redux-action 액션 생성 함수
+export const increase = () => createAction(INCREASE);
+export const decrease = () => createAction(DECREASE);
 
 // 초기 상태 정의
 const initialState = {
@@ -12,19 +18,30 @@ const initialState = {
 };
 
 //리듀서
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+// function counter(state = initialState, action) {
+//   switch (action.type) {
+//     case INCREASE:
+//       return {
+//         number: state.number + 1,
+//       };
+//     case DECREASE:
+//       return {
+//         number: state.number - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+//리듀서 with redux-action handleActions
+const counter = handleActions(
+  // 첫번째 매개변수 : 각 액션에 대한 업데이트 함수
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  // 첫번째 매개변수 : 초기 상태
+  initialState
+);
 
 export default counter;
