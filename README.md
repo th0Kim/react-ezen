88ab1c9866888e0088868802b6d4a2a62541318b
Kim soyoung <3001th0@naver.com>
2023-08-20 14:14:10 +0900

    provider 적용

--- README.md
+++ README.md
@@ -1213,7 +1213,7 @@ unsubscribe();
 5. 리듀서 추가
   - ...state : 기존의 상태를 복사하여 사용하는 이뮤는 불변성 때문
 6. store 생성
-7. subscribe 구독(store의 내장 함수)
+7. subscribe 구독(store의 내장 함수) - 리액트 리덕스에서는 Provider로 사용한다.
 8. 디스패치(store의 내장 함수)
 
 #### 리덕스 세가지 규칙
--- redux/src/modules/index.js
+++ redux/src/modules/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import counter from "./counter";
 import todos from "./todos";
 
+//import rootReducer from './modules'; < index.js 가 modules 디렉토리의 루트 파일이 되어 가능.
 const rootReducer = combineReducers({
   counter,
   todos,
