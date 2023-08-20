0b183d04209f0938313e9db420474c1b3c3712d3
Kim soyoung <3001th0@naver.com>
2023-08-20 10:46:29 +0900

    DOM 노드를 가르키는 값 선언

--- vanilla-redux/index.js
+++ vanilla-redux/index.js
@@ -1,1 +1,5 @@
-console.log("hello parcel");
+// DOM 노드를 가르키는 값 선언
+const divToggle = document.querySelector(".toggle");
+const counter = document.querySelector("h1");
+const btnIncrease = document.querySelector("#increase");
+const btnDecrease = document.querySelector("#decrease");
