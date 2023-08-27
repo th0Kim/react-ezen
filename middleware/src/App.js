5128cc4f50e9e42105705dd5081a61641daa2e8b
Kim soyoung <3001th0@naver.com>
2023-08-27 17:03:48 +0900

    Update App.js

--- middleware/src/App.js
+++ middleware/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import CounterContainer from "./containers/CounterContainer";
+import SampleContainer from "./containers/SampleContainer";
 
 function App() {
   return (
     <div>
       {/* 리덕스에서는 containers 컴포넌트가 중심임 app에 직접 연결하는 파일 */}
       <CounterContainer />
+      <SampleContainer />
     </div>
   );
 }
