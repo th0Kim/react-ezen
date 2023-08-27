a9b74a768bc1ee642c6feb229c033859d7451b5c
Kim soyoung <3001th0@naver.com>
2023-08-27 11:55:51 +0900

    변환: useActions hooks 사용

--- redux/src/containers/TodosContainer.js
+++ redux/src/containers/TodosContainer.js
@@ -39,20 +39,27 @@ import { changeInput, insert, toggle, remove } from "../modules/todos"; //모듈
 // connect함수를 쓰지 않고 useSelector/useDispatch/useCallback Hooks를 사용한 방법으로 리덕스 상태 확인/디스패치
 import React, { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useActions } from "../lib/useActions";
 
 const TodosContainer = () => {
   const { input, todos } = useSelector(({ todos }) => ({
     input: todos.input,
     todos: todos.todos,
   }));
-  const dispatch = useDispatch();
-  const onChangeInput = useCallback(
-    (input) => dispatch(changeInput(input)),
-    [dispatch]
+  // const dispatch = useDispatch();
+  // const onChangeInput = useCallback(
+  //   (input) => dispatch(changeInput(input)),
+  //   [dispatch]
+  // );
+  // const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
+  // const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
+  // const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
+
+  //useActions hooks 사용
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+    [changeInput, insert, toggle, remove],
+    []
   );
-  const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
-  const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
-  const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
 
   return (
     <Todos
