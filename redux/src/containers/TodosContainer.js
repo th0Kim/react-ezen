fe1c43b5dc5aa427c27acd0369e9f09f0c8d05e8
Kim soyoung <3001th0@naver.com>
2023-08-27 11:27:26 +0900

    변환 : connect함수를 쓰지 않고 useSelector/useDispatch/useCallback Hooks를 사용한 방법으로 리덕스 상태 확인/디스패치

--- redux/src/containers/TodosContainer.js
+++ redux/src/containers/TodosContainer.js
@@ -1,36 +1,69 @@
-import { connect } from "react-redux";
+// import { connect } from "react-redux";
 import Todos from "../components/Todo"; //UI
 import { changeInput, insert, toggle, remove } from "../modules/todos"; //모듈의 변수 연결
 
-const TodosContainer = ({
-  input,
-  todos,
-  changeInput,
-  insert,
-  toggle,
-  remove,
-}) => {
+// connect 사용
+// const TodosContainer = ({
+//   input,
+//   todos,
+//   changeInput,
+//   insert,
+//   toggle,
+//   remove,
+// }) => {
+//   return (
+//     <Todos
+//       input={input}
+//       todos={todos}
+//       onChangeInput={changeInput}
+//       onInsert={insert}
+//       onToggle={toggle}
+//       onRemove={remove}
+//     />
+//   );
+// };
+
+// export default connect(
+//   ({ todos }) => ({
+//     input: todos.input,
+//     todos: todos.todos,
+//   }),
+//   {
+//     changeInput,
+//     insert,
+//     toggle,
+//     remove,
+//   }
+// )(TodosContainer);
+
+// connect함수를 쓰지 않고 useSelector/useDispatch/useCallback Hooks를 사용한 방법으로 리덕스 상태 확인/디스패치
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+const TodosContainer = () => {
+  const { input, todos } = useSelector(({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos,
+  }));
+  const dispatch = useDispatch();
+  const onChangeInput = useCallback(
+    (input) => dispatch(changeInput(input)),
+    [dispatch]
+  );
+  const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
+  const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
+  const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
+
   return (
     <Todos
       input={input}
       todos={todos}
-      onChangeInput={changeInput}
-      onInsert={insert}
-      onToggle={toggle}
-      onRemove={remove}
+      onChangeInput={onChangeInput}
+      onInsert={onInsert}
+      onToggle={onToggle}
+      onRemove={onRemove}
     />
   );
 };
 
-export default connect(
-  ({ todos }) => ({
-    input: todos.input,
-    todos: todos.todos,
-  }),
-  {
-    changeInput,
-    insert,
-    toggle,
-    remove,
-  }
-)(TodosContainer);
+export default TodosContainer;
