6885b5bedc726c4347328557d0b0ffef90aa64df
Kim soyoung <3001th0@naver.com>
2023-09-03 11:42:08 +0900

    lodash 라이브러리 사용 해 봄(중복값 제거)

--- nodeLearning/index.js
+++ nodeLearning/index.js
@@ -1,4 +1,11 @@
 // import { PI, getArea, getCircumference } from "./circle.js";
-import circle from "./circle.js"; //모든 값 불러오기
+// import circle from "./circle.js"; //모든 값 불러오기
 
-console.log(circle.PI, circle.getArea(1), circle.getCircumference(1));
+// console.log(circle.PI, circle.getArea(1), circle.getCircumference(1));
+
+import lodash from "lodash";
+
+const arr = [1, 1, 1, 2, 2, 1, 1, 4, 4, 3, 2];
+const uniqueArr = lodash.uniqBy(arr); //중복값 제거
+
+console.log(uniqueArr);
