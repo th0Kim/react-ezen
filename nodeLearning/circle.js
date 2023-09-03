0f18710778deb84861d1be2831bef0e6ac70b135
Kim soyoung <3001th0@naver.com>
2023-09-03 11:14:10 +0900

    ESM문법 내보내기

---
+++ nodeLearning/circle.js
@@ -0,0 +1,12 @@
+//원의 둘레 공식
+const PI = 3.141592;
+
+function getArea(radius) {
+  return PI * radius * radius;
+}
+
+function getCircumference(radius) {
+  return 2 * PI * radius;
+}
+
+export { PI, getArea, getCircumference }; //다른 파일에서 사용 할 수 있도록 내보내기
--- nodeLearning/index.js
+++ nodeLearning/index.js
@@ -1,1 +1,3 @@
-console.log("index run");
+import { PI, getArea, getCircumference } from "./circle.js";
+
+console.log(PI, getArea(1), getCircumference(1));
